eaa548557a9e4e51333b428def10d6562b272de2
Jonathon Nagy <nagy.jon93@gmail.com>
2021-12-02 23:24:30 +0100

    update

--- main.js
+++ main.js
@@ -20,15 +20,15 @@ functionName(1,2);
 
  */
 
-const inputElement = (type, name, title) =>{
+const inputElement = (type, name, title, req = '') => {
     return `
-        <div>
+        <div class='${type}'>
             <label for='${name}'>${title}</label>
-            <input type='${type}' name='${name}'>
+            <input type='${type}' name='${name}' ${req}>
         </div>
     `;
 }
-const selectElement = (type, name, title, options) =>{
+const selectElement = (type, name, title, options) => {
     let optionsToSelect = '';
     for (const o of options) {
         optionsToSelect += `
@@ -37,8 +37,8 @@ const selectElement = (type, name, title, options) =>{
             </option>
         `;
     }
-    console.log(optionsToSelect)
-    
+    // console.log(optionsToSelect)
+
     return `
         <div>
           <label for='${name}'>${title}</label>
@@ -54,45 +54,133 @@ const selectElement = (type, name, title, options) =>{
 */
 // const test = 'valami' 'valami2'
 
-const formElement = `
-    <form id='form'>
-        ${inputElement('text', 'firstName', 'Keresztneved')}
-        ${inputElement('email', 'personalEmail', 'Email címed')}
-        ${inputElement('file', 'profilePicture', 'Profilképed')}
-        ${inputElement('checkbox', 'newsLetter', 'Hírlevelet szeretnél kapni')}
-        ${inputElement('checkbox', 'terms', 'Elfogadom a felhasználási feltételeket')}
+// const nameData = {
+//     type: 'text',
+//     name: 'firstName',
+//     label: 'Keresztneved'
+// }
+
+const formFields = [
+    {
+        type: 'text',
+        name: 'firstName',
+        label: 'Keresztneved'
+    },
+    {
+        type: 'email',
+        name: 'personalEmail',
+        label: 'Email cimed',
+        req: 'required'
+    },
+    {
+        type: 'file',
+        name: 'profilePicture',
+        label: 'Profilkeped'
+    },
+    {
+        type: 'checkbox',
+        name: 'newsLetter',
+        label: 'Hírlevelet szeretnél kapni'
+    },
+    {
+        type: 'checkbox',
+        name: 'terms',
+        label: 'Elfogadom a felhasználási feltételeket'
+    }
+]
+
+const anotherFormFields = [
+    {
+        type: 'text',
+        name: 'street',
+        label: 'kozterulet'
+    },
+    {
+        type: 'number',
+        name: 'houseNumber',
+        label: 'hazszam'
+    },
+    {
+        type: 'number',
+        name: 'zipcode',
+        label: 'Iranyitoszam'
+    },
+    {
+        type: 'text',
+        name: 'city',
+        label: 'Telepules neve'
+    }
+];
+
+
+
+// const formElement = `
+//     <form id='form'>
+//         ${inputElement(nameData.type, nameData.name, nameData.label)}
+//         ${inputElement('email', 'personalEmail', 'Email címed', 'required')}
+//         ${inputElement('file', 'profilePicture', 'Profilképed')}
+//         ${inputElement('checkbox', 'newsLetter', 'Hírlevelet szeretnél kapni')}
+//         ${inputElement('checkbox', 'terms', 'Elfogadom a felhasználási feltételeket')}
+//         ${selectElement('select', 'where', 'Hol hallotal rolunk?',['interneten', 'ismerostol', 'egyeb'])}
+//         <button>Ok</button>
+//     </form>
+// `;
+
+const formElement = (ffs, id) => {
+    let inputs = '';
+
+    for (const ff of ffs) {
+        inputs += inputElement(ff.type, ff.name, ff.label, ff.req)
+    }
+    return `
+    <form id='${id}'>
+        ${inputs}
         ${selectElement('select', 'where', 'Hol hallotal rolunk?',['interneten', 'ismerostol', 'egyeb'])}
-        <button>Ok</button>
+        <button>Küldés</button>
     </form>
-`;
-const formSubmit = (event) =>{
+    `;
+}
+
+const formSubmit = (event) => {
     event.preventDefault();
     const et = event.target;
     console.log(et)
     event.target.classList.add('submitted');
+
     let selectValue = et.querySelector('select[name="where"]').value;
+
     console.log(selectValue)
 }
 
-const inputUpdate = (event) =>{
+const inputUpdate = (event) => {
     if (event.target.getAttribute('name') === 'firstName') {
         document.getElementById('inputValue').innerHTML = event.target.value;
+    }
+    if (event.target.getAttribute('name') === 'profilePicture') {
+        console.log(event.target.files[0]);
+
+        const image = URL.createObjectURL(event.target.files[0]);
+        document.getElementById('inputValue').insertAdjacentHTML("beforeend", `
+        <img src='${image}'>
+        `)
     }
     console.log(event.target.closest('#form'))
 }
 
 function loadEvent() {
     const root = document.getElementById('root');
-    root.insertAdjacentHTML('afterbegin', formElement);
+    root.insertAdjacentHTML('afterbegin', formElement(anotherFormFields, 'form'));
     root.insertAdjacentHTML('afterbegin', `
         <div id='inputValue'></div>
     `);
+    root.insertAdjacentHTML('afterbegin', formElement(formFields, 'form'));
+    
 
     const form = document.getElementById('form');
     form.addEventListener('submit', formSubmit)
-    
+
     const inputList = form.querySelectorAll('input');
-    
+
     for (const input of inputList) {
         input.addEventListener('input', inputUpdate)
     }
